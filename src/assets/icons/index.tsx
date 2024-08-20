93155f0221844ac1c46c7de7bb85203bd0e984c7
enmanuel-mctekk <enmanuel@mctekk.com>
2024-08-20 13:12:15 -0400

    feat(register): update icons

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -5,3 +5,4 @@ export { default as AppleIcon } from './apple-icon';
 export { default as FacebookIcon } from './facebook-icon';
 export { default as EmailIcon } from './email-icon';
 export { default as Check } from './check';
+export { default as NextArrow } from './next-arrow';
