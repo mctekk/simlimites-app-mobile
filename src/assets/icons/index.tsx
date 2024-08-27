655928a1ee5de41ee0272a3cd70982c0858f5e8e
enmanuel-mctekk <enmanuel@mctekk.com>
2024-08-27 13:57:24 -0400

    feat(home): add search icon

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -4,6 +4,7 @@ export { default as GoogleIcon } from './google-icon';
 export { default as AppleIcon } from './apple-icon';
 export { default as FacebookIcon } from './facebook-icon';
 export { default as EmailIcon } from './email-icon';
+export { default as SearchIcon } from './search-icon';
 export { default as Check } from './check';
 export { default as NextArrow } from './next-arrow';
 export { default as HomeIcon } from './home-icon';
