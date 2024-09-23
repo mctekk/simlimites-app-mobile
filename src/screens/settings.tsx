af6d8c3c3c664cec4a517258f20d35f3ae801eb9
enmanuel-mctekk <enmanuel@mctekk.com>
2024-09-23 15:18:12 -0400

    feat(profile): update settings

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,30 +1,32 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
-/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react-hooks/exhaustive-deps */
 
 // Modules
-import React, {useCallback, useContext, useEffect} from 'react';
-import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
-import styled from 'styled-components';
-
-// Icons
-import MenuIcon from 'assets/icons/menu-icon';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import styled from 'styled-components/native';
+import { SectionList, SafeAreaView } from 'react-native';
 
 // Atoms
-import Button from 'components/atoms/button';
 import { TextTransform, translate } from 'components/atoms/localized-label';
+import SectionHeader from 'components/atoms/section-header';
+import { OrdersIcon, EditIcon, MyeSimsIcon } from 'assets/icons';
+import CustomText from 'atoms/text';
 
 // Context
-import {AuthContext} from 'components/context/auth-context';
-import {UserContext} from 'components/context/user-context';
+import { AuthContext } from 'components/context/auth-context';
+import { UserContext } from 'components/context/user-context';
 
 // Molecules
 import Header from 'components/molecules/header';
-import {SettingsItems} from 'components/molecules/settings-items';
+import SettingsItem from 'components/molecules/settings-item';
 
 // Styles
-import {Colors, Typography} from 'styles';
+import { Typography } from 'styles';
 import { DEFAULT_THEME } from 'styles/theme';
+
+// Utils
+import { isIphoneX } from 'utils/iphone-helpers';
 
 // Interfaces
 interface ISettingsProps {
@@ -34,60 +36,111 @@ interface ISettingsProps {
 const Container = styled.View`
   flex: 1;
   background-color: ${DEFAULT_THEME.background};
+  padding-horizontal: 16px;
 `;
 
 const ScreenHeader = styled(Header)`
-  justify-content: space-between;
   align-items: center;
-  background-color: ${DEFAULT_THEME.primary};
+  background-color: ${DEFAULT_THEME.background};
+  padding-top: 10px;
+  height: 60px;
+  margin-bottom: 0px;
+  padding-horizontal: 22px;
 `;
 
-const Content = styled.SafeAreaView`
+const Content = styled.ScrollView`
   flex: 1px;
-  margin: 10px;
 `;
 
-const IconContainer = styled.TouchableOpacity`
-  align-items: center;
-  padding-right: 16px;
-  margin-top: 10px;
+const ListFooterContainer = styled.View`
+  margin-top: ${isIphoneX() ? 45 : 0}px;
+  height: 100px;
+  background-color: ${DEFAULT_THEME.background};
 `;
 
-const LogoutButton = styled(Button)`
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 30px;
-  height: 50px;
-  width: 50%;
+const FooterContainer = styled.View`
+  bottom: ${isIphoneX() ? 40 : 100}px;
+`;
+
+const FooterButton = styled.TouchableOpacity`
+  margin-vertical: 20px;
+`;
+
+const EditButton = styled.TouchableOpacity`
+  background-color: ${DEFAULT_THEME.primary};
   align-self: center;
-  border-radius: 10px;
-  background-color: ${DEFAULT_THEME.error};
+  border-radius: 8px;
+  padding-vertical: 6px;
+  padding-horizontal: 16px;
+  align-items: center;
+  margin-bottom: 58px;
+  flex-direction: row;
 `;
 
-const data = [
+export const SettingsItemList = [
   {
-    key: 1,
-    name: translate('editProfile', TextTransform.CAPITALIZE),
-    goTo: 'EditProfile',
+    title: translate('settings', TextTransform.CAPITAL),
+    data: [
+      {
+        title: translate('myPlans', TextTransform.CAPITALIZE),
+        key: 'myPlans',
+        goTo: '',
+        icon: () => <MyeSimsIcon size={18} />,
+      },
+      {
+        title: translate('myOrders', TextTransform.CAPITALIZE),
+        key: 'myOrders',
+        goTo: '',
+        icon: () => <OrdersIcon />,
+      },
+    ],
   },
   {
-    key: 2,
-    name: translate('changePassword', TextTransform.CAPITALIZE),
-    goTo: 'ChangePassword',
+    title: translate('account', TextTransform.CAPITAL),
+    data: [
+      {
+        title: translate('changePassword', TextTransform.CAPITALIZE),
+        key: 'changePassword',
+        goTo: 'ChangePassword',
+      },
+    ],
+    headerStyle: {
+      marginTop: 50,
+    },
+  },
+  {
+    title: translate('help', TextTransform.CAPITAL),
+    data: [
+      {
+        title: translate('userGuide', TextTransform.CAPITALIZE),
+        key: 'userGuide',
+        goTo: '',
+      },
+      {
+        title: translate('aboutUs', TextTransform.CAPITALIZE),
+        key: 'aboutUs',
+        goTo: '',
+      },
+      {
+        title: translate('termsAndConditions', TextTransform.CAPITALIZE),
+        key: 'termsAndConditions',
+        goTo: '',
+      },
+    ],
+    headerStyle: {
+      marginTop: 50,
+    },
   },
 ];
 
+
 export const Settings = (props: ISettingsProps) => {
   // Props
-  const {navigation} = props;
+  const { navigation } = props;
 
   // Context
-  const {signOut} = useContext(AuthContext);
-  const {userData} = useContext(UserContext);
-
-  useEffect(() => {
-    console.log('User Data:', userData);
-  }, []);
+  const { signOut } = useContext(AuthContext);
+  const { userData } = useContext(UserContext);
 
   const handleLogout = async () => {
     try {
@@ -97,46 +150,92 @@ export const Settings = (props: ISettingsProps) => {
     }
   };
 
-  const LeftButtonComponent = () => (
-    <IconContainer onPress={() => navigation.openDrawer()}>
-      <MenuIcon />
-    </IconContainer>
-  );
+  const renderItem = useCallback(({ item, index, section }) => {
+    const isFirst = index === 0;
+    const isLast = index === (section?.data?.length - 1);
 
-  const renderItem = useCallback(({item}) => {
     return (
-      <SettingsItems
-        key={item.key}
-        name={item.name}
-        onPress={() => navigation.navigate(item.goTo)}
+      <SettingsItem
+        key={index}
+        title={item.title}
+        icon={item.icon}
+        onPress={() => navigation.navigate(item.goTo ?? '')}
+        isFirst={isFirst}
+        isLast={isLast}
       />
     );
+  }, []);
+
+  const renderSectionHeader = useCallback(({ section: { title, headerStyle } }) => {
+    return (
+      <SectionHeader
+        title={title}
+        style={headerStyle}
+      />
+    );
+  }, []);
+
+  const ListFooterComponent = useCallback(() => {
+    return (
+      <ListFooterContainer>
+        <FooterContainer>
+          <FooterButton
+            onPress={handleLogout}
+          >
+            <SettingsItem
+              title={translate('logout', TextTransform.CAPITALIZE)}
+              onPress={handleLogout}
+              showRightIcon={false}
+              isFirst
+              isLast
+            />
+          </FooterButton>
+        </FooterContainer>
+
+      </ListFooterContainer>
+    )
   }, []);
 
   const keyExtractor = useCallback(item => item.key.toString(), []);
 
   return (
     <Container>
+      <SafeAreaView />
       <ScreenHeader
-        title={translate('settings', TextTransform.CAPITALIZE)}
-        leftButtonComponent={<></>}
+        hasBackButton={false}
+        title={translate('profile', TextTransform.CAPITALIZE)}
+        titleProps={{
+          style: {
+            textAlign: 'center',
+            fontSize: 15,
+            fontWeight: '600',
+            width: '100%',
+          },
+        }}
       />
-
       <Content>
-        <FlatList
-          data={data}
-          extraData={data}
+        <EditButton onPress={() => navigation.navigate('EditProfile')}>
+          <EditIcon />
+          <CustomText
+            color={DEFAULT_THEME.white}
+            size={Typography.FONT_SIZE_13}
+            weight='500'
+            style={{ marginLeft: 5 }}
+          >
+            {translate('editProfile', TextTransform.CAPITALIZE)}
+          </CustomText>
+        </EditButton>
+        <SectionList
+          sections={SettingsItemList}
+          extraData={SettingsItemList}
+          renderSectionHeader={renderSectionHeader}
           renderItem={renderItem}
           keyExtractor={keyExtractor}
-        />
-
-        <LogoutButton
-          onPress={handleLogout}
-          title={translate('logout', TextTransform.CAPITALIZE)}
-          textStyle={{
-            color: Colors.WHITE,
-            fontSize: Typography.FONT_SIZE_16,
-            fontWeight: 'bold',
+          showsVerticalScrollIndicator={false}
+          scrollEnabled={false}
+          ListFooterComponent={ListFooterComponent}
+          style={{
+            backgroundColor: DEFAULT_THEME.background,
           }}
         />
       </Content>
