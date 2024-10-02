27e7e4b26d56b0370074c530d37605123200f7ee
enmanuel-mctekk <enmanuel@mctekk.com>
2024-10-02 13:49:52 -0400

    feat(checkut): update variant card

--- src/components/molecules/variant-card.tsx
+++ src/components/molecules/variant-card.tsx
@@ -1,28 +1,32 @@
-
 // Modules
-import React from "react";
-import styled from "styled-components/native";
+import React, { useEffect } from 'react';
+import styled from 'styled-components/native';
 
 // Atoms
 import CustomText from 'atoms/text';
 import { TextTransform, translate } from 'components/atoms/localized-label';
 
 // Styles
-import { Typography } from "styles";
-import { DEFAULT_THEME } from "styles/theme";
+import { Typography } from 'styles';
+import { DEFAULT_THEME } from 'styles/theme';
 
 // Utils
 import { isIphoneSE, isIphone14 } from 'utils/iphone-helpers';
 
+// Interface
+import { VariantInterface, AttributesInterface } from '@kanvas/core';
+
+// Contants
+import { VARIANT_ATTRIBUTE_SLUGS } from 'utils/constants';
+
 interface IVariantCardProps {
   onPress?: () => void;
-  label: string;
-  price?: string | number;
   isSelected?: boolean;
+  variant?: VariantInterface;
 }
 
 const Container = styled.TouchableOpacity`
-  backgroundColor: ${DEFAULT_THEME.authBackground};
+  background-color: ${DEFAULT_THEME.authBackground};
   padding-horizontal: 20px;
   padding-vertical: 17px;
   margin-bottom: 8px;
@@ -32,39 +36,50 @@ const Container = styled.TouchableOpacity`
 `;
 
 const VariantCard = (props: IVariantCardProps) => {
-
-  const { onPress, label, price, isSelected } = props;
+  const { onPress, isSelected, variant } = props;
 
   const onCardPress = () => {
     onPress?.();
   };
 
+  const getVariantDuration = () => {
+    let variantDuration = '';
+    const durationAttribute = variant?.attributes?.find(
+      (attribute: any) => attribute?.slug === VARIANT_ATTRIBUTE_SLUGS.DURATION_SLUG,
+    );
+
+    if (durationAttribute) {
+      variantDuration = durationAttribute?.value;
+    }
+
+    return variantDuration;
+  };
+
   return (
     <Container
       onPress={onCardPress}
-      style={{ borderColor: isSelected ? DEFAULT_THEME.black : DEFAULT_THEME.transparent }}
-    >
+      style={{ borderColor: isSelected ? DEFAULT_THEME.black : DEFAULT_THEME.transparent }}>
       <CustomText
-        size={(isIphoneSE() || isIphone14()) ? Typography.FONT_SIZE_22 : Typography.FONT_SIZE_28}
+        size={isIphoneSE() || isIphone14() ? Typography.FONT_SIZE_22 : Typography.FONT_SIZE_28}
         weight='700'
         style={{ marginBottom: 1 }}
         color={DEFAULT_THEME.title}>
-        {label}
+        {variant?.name}
       </CustomText>
       <CustomText
         size={Typography.FONT_SIZE_12}
         style={{ marginBottom: 24 }}
         color={DEFAULT_THEME.variantSubtitle}>
-        {translate('dataOnly', TextTransform.CAPITAL)}
+        {`${getVariantDuration()} ${translate('days', TextTransform.NONE)}`}
       </CustomText>
       <CustomText
         size={Typography.FONT_SIZE_15}
         weight='700'
         color={DEFAULT_THEME.variantSubtitle}>
-        {`$${price}.00`}
+        {`$${variant?.channel?.price}.00`}
       </CustomText>
     </Container>
-  )
+  );
 };
 
 export default VariantCard;
